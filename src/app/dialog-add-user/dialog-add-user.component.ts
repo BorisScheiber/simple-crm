b1d0d61772404e6fbca909fe498f399aacd00a6c
Boris Scheiber <scheiberboris@hotmail.com>
2025-01-03 21:53:05 +0100

    worked on dialog-add-user

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { materialModules } from '../shared/material/material';
+import { User } from '../models/user.class';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
-  imports: [materialModules],
+  imports: [materialModules, FormsModule],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss'
 })
 export class DialogAddUserComponent {
+ user: User = new User();
 
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { materialModules } from '../shared/material/material';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
+import { User } from '../models/user.class';
 
 @Component({
   selector: 'app-user',
@@ -11,6 +12,8 @@ import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.compo
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
+
+  user = new User();
 
   constructor(public dialog: MatDialog) {}
 
