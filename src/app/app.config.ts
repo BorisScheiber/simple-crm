5520b8bc9e1db68ca3af9aa110ba81ee66f652ce
Boris Scheiber <scheiberboris@hotmail.com>
2025-01-05 17:01:35 +0100

    updated api key security

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,20 @@ import { provideNativeDateAdapter } from '@angular/material/core';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { environment } from '../enviroments/enviroment';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), provideNativeDateAdapter(), provideFirebaseApp(() => initializeApp({"projectId":"simple-crm-27075","appId":"1:1026678219764:web:825d238bfd99267f600fa1","storageBucket":"simple-crm-27075.firebasestorage.app","apiKey":"AIzaSyDP-d7xKq8KoBP1XZudL0yD1r5hs6nYqXk","authDomain":"simple-crm-27075.firebaseapp.com","messagingSenderId":"1026678219764"})), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase())]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideNativeDateAdapter(),
+    provideFirebaseApp(() =>
+      initializeApp(environment.firebase) // Verwende die Firebase-Konfiguration aus `environment.ts`
+    ),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+  ],
 };
---
+++ src/enviroments/enviroment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false, // Gibt an, dass dies die Entwicklungsumgebung ist
+    firebase: {
+      apiKey: 'AIzaSyDP-d7xKq8KoBP1XZudL0yD1r5hs6nYqXk',
+      authDomain: 'simple-crm-27075.firebaseapp.com',
+      projectId: 'simple-crm-27075',
+      storageBucket: 'simple-crm-27075.firebasestorage.app',
+      messagingSenderId: '1026678219764',
+      appId: '1:1026678219764:web:825d238bfd99267f600fa1',
+    },
+  };
